03e45470530527e3eea79986ae27d9f48bf02379
Kevin Baza <kbaza@teamraft.com>
2024-07-26 08:38:20 -0400

    Removed unused methods.

--- engine/src/main/java/com/arcadedb/security/AuthorizationUtils.java
+++ engine/src/main/java/com/arcadedb/security/AuthorizationUtils.java
@@ -1,8 +1,6 @@
 package com.arcadedb.security;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.logging.Level;
 
 import com.arcadedb.database.Document;
@@ -10,13 +8,7 @@ import com.arcadedb.database.MutableDocument;
 import com.arcadedb.database.EmbeddedDatabase.RecordAction;
 import com.arcadedb.exception.ValidationException;
 import com.arcadedb.log.LogManager;
-import com.arcadedb.serializer.json.JSONArray;
 import com.arcadedb.serializer.json.JSONObject;
-import com.arcadedb.security.ACCM.Argument;
-import com.arcadedb.security.ACCM.ArgumentOperator;
-import com.arcadedb.security.ACCM.Expression;
-import com.arcadedb.security.ACCM.ExpressionOperator;
-import com.arcadedb.security.ACCM.GraphType;
 import com.arcadedb.security.ACCM.TypeRestriction;
 
 public class AuthorizationUtils {
@@ -28,15 +20,6 @@ public class AuthorizationUtils {
 
   // todo move to opa since it needs to be configurable
   public static final Map<String, Integer> classificationOptions = Map.of("U", 0, "CUI", 1, "C", 2, "S", 3, "TS", 4);
-
-  // private static TypeRestriction getTypeRestriction() {
-  //   Argument classificationArg = new Argument("classificationTest", ArgumentOperator.ANY_OF, new String[]{"U", "S"});
-  //   Argument releasableToArg = new Argument("releaseableTo", ArgumentOperator.ANY_IN, new String[]{"USA"});
-
-  //   Expression expression = new Expression(ExpressionOperator.AND, classificationArg, releasableToArg);
-  //   TypeRestriction typeRestriction = new TypeRestriction("beta", GraphType.VERTEX, List.of(expression), List.of(expression), List.of(expression), List.of(expression));
-  //   return typeRestriction;
-  // }
 
   /**
    * Checks if the provided classification is permitted given the deployment classification.
@@ -58,135 +41,6 @@ public class AuthorizationUtils {
 
     var deploymentClassification = System.getProperty("deploymentClassification", "S");
     return classificationOptions.get(deploymentClassification) >= classificationOptions.get(c);
-  }
-
-  /**
-   * Checks if the user is authorized to view the resource, taking into account the user's clearance, attributes, the full resouce ACCM markings.
-   * @param userClearance
-   * @param nationality
-   * @param resourceClassification
-   * @return
-   */
-  // public static boolean isUserAuthorizedForResourceMarking(final String userClearance, final String nationality, final String tetragraphs,
-  //       final String resourceClassification) {
-    
-  //   // TODO are tetrapgrahs that group countries auto applicable to users of those countires, or do users need explicit authoirzation for their data?
-  //   var processedResourceClassification = resourceClassification.replace("FVEY", "USA,AUS,CAN,GBR,NZL");
-  //   if (!isClearanceAuthorized(userClearance, processedResourceClassification)) {
-  //     // System.out.println("blocked by clearance");
-  //     return false;
-  //   }
-
-  //   // NO FORN takes precedence over REL TO?
-  //   if (isBlockedByNoForn(nationality, processedResourceClassification)) {
-  //     // System.out.println("blocked by noforn");
-  //     return false;
-  //   }
-
-  //   if (isBlockedByReleaseableTo(nationality, tetragraphs, processedResourceClassification)) {
-  //     // System.out.println("blocked by noforn");
-  //     return false;
-  //   }
-
-  //   return true;
-  // }
-
-  /**
-   * Checks if the user has sufficient clearance to view the resource, outside of any other ACCM restrictions.
-   * @param userClearance
-   * @param resourceClassificationMarking
-   * @return
-   */
-  // public static boolean isClearanceAuthorized(final String userClearance, final String resourceClassificationMarking) {
-  //   if (userClearance == null || userClearance.isEmpty())
-  //     return false;
-
-  //   String processedUserClearance = userClearance.toUpperCase();
-  //   processedUserClearance = processedUserClearance.trim();
-
-  //   String processedResourceClearance = getClassificationFromResourceMarkings(resourceClassificationMarking);
-  //   processedResourceClearance = processedResourceClearance.trim();
-
-  //   if (!classificationOptions.containsKey(processedUserClearance))
-  //     throw new IllegalArgumentException("Clearance must be one of " + classificationOptions);
-
-  //   if (resourceClassificationMarking == null || resourceClassificationMarking.isEmpty())
-  //     return false;
-
-  //   if (!classificationOptions.containsKey(processedResourceClearance))
-  //     throw new IllegalArgumentException("Invalid resource classification " + processedResourceClearance);
-
-  //   return classificationOptions.get(processedUserClearance) >= classificationOptions.get(processedResourceClearance);
-  // }
-
-  /**
-   * Checks if the classification markings contains a releaseable to block, and if so, checks if the user
-   * belongs to an allowable nationality.
-   * @param nationality
-   * @return
-   */
-  // private static boolean isBlockedByReleaseableTo(final String nationality, final String tetragraphs, 
-  //       final String resourceClassificationMarkings) {
-  //   // TODO add support for banner barking AUTHORIZED FOR RELEASE TO
-  //   if (resourceClassificationMarkings.contains("REL TO")) {
-  //       if (nationality == null || nationality.isEmpty()) {
-  //         return true;
-  //       }
-
-  //       var releaseableTo = resourceClassificationMarkings.substring(resourceClassificationMarkings.indexOf("REL TO"));
-  //       if (releaseableTo.contains("//")) {
-  //           releaseableTo.substring(0, releaseableTo.indexOf("//"));
-  //       }
-
-  //       releaseableTo = releaseableTo.substring("REL TO".length() + 1);
-  //       releaseableTo = releaseableTo.replaceAll(" ", "");
-
-  //       if (releaseableTo.contains(",")) {
-  //           return !Set.of(releaseableTo.split(",")).stream().map(r -> r.toString()).anyMatch(r -> {
-  //             if (r.trim().isEmpty()) {
-  //               return false;
-  //             } else if (r.length() == 3) {
-  //               return r.equals(nationality);
-  //             } else if (tetragraphs != null && !tetragraphs.isEmpty() && r.length() == 4) {
-  //               return tetragraphs.contains(r);
-  //             }
-  //             return false;
-  //           });
-  //       } else {
-  //           return !releaseableTo.equals(nationality);
-  //       }
-  //   }
-
-  //   return false;
-  // }
-
-  /**
-   * Checks if the user is unable to see the resource due to a NOFORN marking.
-   * @param nationality
-   * @param resourceClassification
-   * @return
-   */
-  // private static boolean isBlockedByNoForn(final String nationality, final String resourceClassification) {
-  //   return (containsBlockText("NOFORN", resourceClassification) || containsBlockText("NF", resourceClassification)) 
-  //       && (nationality == null || !nationality.equals("USA"));
-  // }
-
-  /**
-   * Checks if the resource classification markings contains the text between single forward slash blocks.
-   * @param text
-   * @param resourceClassification
-   * @return
-   */
-  private static boolean containsBlockText(final String text, final String resourceClassification) {
-    if (resourceClassification.contains("/")) {
-      return Set.of(resourceClassification.split("/")).contains(text);
-    } else if (resourceClassification.equals(text)) {
-      return true;
-    } else {
-      // if the string ends with the text, or starts with the text with a space after it, or contains the text with a space before and after it
-      return resourceClassification.endsWith(" " + text) || resourceClassification.endsWith("-" + text) 
-          || resourceClassification.startsWith(text + " ") || resourceClassification.contains(" " + text + " ");
-    }
   }
 
   /**
@@ -221,10 +75,6 @@ public class AuthorizationUtils {
     return classification;
   }
 
-  public static boolean checkPermissionsOnDocumentToCreate(final Document document, final SecurityDatabaseUser currentUser) {
-    return checkPermissionsOnDocument(document, currentUser, RecordAction.CREATE);
-  }
-
   public static boolean checkPermissionsOnDocumentToRead(final Document document, final SecurityDatabaseUser currentUser) {
     // log duration in ns
     long startTime = System.nanoTime();
@@ -235,14 +85,6 @@ public class AuthorizationUtils {
     LogManager.instance().log(AuthorizationUtils.class, Level.INFO, "checkPermissionsOnDocumentToRead took " + duration + " ns");
 
     return result;
-  }
-
-  public static boolean checkPermissionsOnDocumentToUpdate(final Document document, final SecurityDatabaseUser currentUser) {
-    return checkPermissionsOnDocument(document, currentUser, RecordAction.UPDATE);
-  }
-
-  public static boolean checkPermissionsOnDocumentToDelete(final Document document, final SecurityDatabaseUser currentUser) {
-    return checkPermissionsOnDocument(document, currentUser, RecordAction.DELETE);
   }
 
   // split out crud actions
