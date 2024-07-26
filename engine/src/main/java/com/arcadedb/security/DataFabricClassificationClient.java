fcbd31c47cc456866406db733c6ddf785876ba9c
Kevin Baza <kbaza@teamraft.com>
2024-07-26 12:49:53 -0400

    Moved dfClassificationClient to arcadedb-engine

--- engine/src/main/java/com/arcadedb/security/DataFabricClassificationClient.java
+++ engine/src/main/java/com/arcadedb/security/DataFabricClassificationClient.java
@@ -1,7 +1,6 @@
-package com.arcadedb.server;
+package com.arcadedb.security;
 
 import com.arcadedb.GlobalConfiguration;
-import com.arcadedb.database.DocumentValidator;
 import com.arcadedb.log.LogManager;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
