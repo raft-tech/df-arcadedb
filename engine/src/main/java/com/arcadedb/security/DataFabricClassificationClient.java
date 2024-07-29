d3731996afeddfc897fb5fc66aaf1ca161f2abdf
Kevin Baza <kbaza@teamraft.com>
2024-07-29 06:22:17 -0400

    Rewrote df classification client.

--- engine/src/main/java/com/arcadedb/security/DataFabricClassificationClient.java
+++ engine/src/main/java/com/arcadedb/security/DataFabricClassificationClient.java
@@ -1,16 +1,13 @@
 package com.arcadedb.security;
 
 import com.arcadedb.GlobalConfiguration;
-import com.arcadedb.database.DocumentValidator;
 import com.arcadedb.log.LogManager;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.logging.Level;
 
 public class DataFabricClassificationClient {
@@ -23,27 +20,39 @@ public class DataFabricClassificationClient {
      * @return true if the classification structure is valid and meets classification level of the data fabric deployment
      */
     public static boolean validateDocumentClassification(String classification) {
-        String url = String.format("%s/api/v1/classification/validate", GlobalConfiguration.DF_CLASSIFICATION_URL.getValueAsString());
+        String urlString = String.format("%s/api/v1/classification/validate", GlobalConfiguration.DF_CLASSIFICATION_URL.getValueAsString());
+
+        URL url = null;
         try {
-            HttpClient client = HttpClient.newHttpClient();
-
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .POST(HttpRequest.BodyPublishers.ofString(new ObjectMapper().writeValueAsString(classification)))
-                    .build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-            if (response.statusCode() == 200) {
-                return true;
-            }
-        } catch (JsonProcessingException e) {
-            LogManager.instance().log(DataFabricClassificationClient.class, Level.WARNING, "Failed to parse classification!");
+            url = new URL(urlString);
+            LogManager.instance().log(DataFabricClassificationClient.class, Level.INFO, "Validating " + classification);
+            LogManager.instance().log(DataFabricClassificationClient.class, Level.INFO, "Validation URL " + urlString);
+        } catch (MalformedURLException e) {
             throw new RuntimeException(e);
-        } catch (IOException | InterruptedException e) {
-            LogManager.instance().log(DataFabricClassificationClient.class, Level.WARNING, "Failed to send validation request!");
+        }
+        try {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = conn.getOutputStream();
+            os.write(classification.getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                LogManager.instance().log(
+                        DataFabricClassificationClient.class,
+                        Level.WARNING,
+                        "Validation failed!");
+                return false;
+            }
+
+            conn.disconnect();
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        return false;
+        return true;
     }
 }
