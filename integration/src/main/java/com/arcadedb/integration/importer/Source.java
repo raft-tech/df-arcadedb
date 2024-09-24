e19f44870d109ddaee8d55bb7a1e803d24669ac7
justin <jdowdy@teamraft.com>
2024-09-24 15:21:47 +0000

    Revert "Revert "wip""
    This reverts commit c0ec24bd82f902b9dd666c3bfb36d58969b0db53.

--- integration/src/main/java/com/arcadedb/integration/importer/Source.java
+++ integration/src/main/java/com/arcadedb/integration/importer/Source.java
@@ -26,6 +26,7 @@ import java.util.logging.*;
 
 public class Source {
   public final  String                                      url;
+  public String                                      urlWithoutParameters;
   public        InputStream                                 inputStream;
   public final  long                                        totalSize;
   public final  boolean                                     compressed;
@@ -35,6 +36,8 @@ public class Source {
   public Source(final String url, final InputStream inputStream, final long totalSize, final boolean compressed,
       final com.arcadedb.utility.Callable<Void, Source> resetCallback, final Callable<Void> closeCallback) {
     this.url = url;
+    this.urlWithoutParameters = url.replaceAll("[?][^?]*",""); // sometimes urls have params like 'http://bob.com/thing?name=jo'
+                                                               // and we sometimes don't need those params
     this.inputStream = inputStream;
     this.totalSize = totalSize;
     this.compressed = compressed;
